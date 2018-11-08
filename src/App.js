889b204fc6eccf9c99cf84e31a991363bec159ad
CJ Romero <cjromero51@yahoo.com>
2018-11-07 17:00:04 -0800

    added responsive css stylesheet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './responsive.css'
 import Section from './components/section';
 import Main from './components/main'
 
--- src/components/projectList.js
+++ src/components/projectList.js
@@ -7,8 +7,11 @@ class ProjectList extends Component {
         {this.props && (
           this.props.projectObjects.map((project, id) => (
             <div key={id} className='project_container'>
-              <a href={project.url}>
-                <img className='project_image' src={project.image} srcSet={`${project.image1x} 1x, ${project.image2x} 2x`} alt={project.name}/>
+              <a className='image_container' href={project.url}>
+                <img className='project_image'
+                  src={project.image}
+                  srcSet={`${project.image1x} 1x, ${project.image2x} 2x`}
+                  alt={project.name}/>
                 </a>
                 <a className='project_item' href={project.url}>
                   <h3 className='project_titles'>{project.name}</h3>
