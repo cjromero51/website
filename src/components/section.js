9b1fb9deb9942abeaf89e70705b3ce9733aad6e0
CJ Romero <cjromero51@yahoo.com>
2018-11-08 11:25:40 -0800

    font changes to front page

--- src/components/section.js
+++ src/components/section.js
@@ -5,15 +5,20 @@ class Section extends Component {
     return (
       <section className="header_full">
         <header className="header">
+          <nav>
+            <a className="nav-link" href="#">About Me</a>
+            <a className="nav-link" href="#">Web</a>
+            <a className="nav-link" href="#">Contact</a>
+          </nav>
+          <hr className="header_line"/>
           <div className="header_interior">
-            <img src="./images/udacity.svg" alt="logo" className="udacity_logo"/>
-            <h1 className="header_title">
-              CJ ROMERO
-            </h1>
-            <h2 className="title_description">
-              FRONT-END WEB DEVELOPER
-            </h2>
-            <hr className="header_line"/>
+            <p>Greetings.</p>
+            <p>
+              My name is CJ Romero, an aspiring front-end web developer looking to invest a ton of time and effort into the field.  I currently reside in Santa Clarita, CA, just outside of Los Angeles
+            </p>
+            <p>My strengths include javaScript, HTML, and CSS.  I have familiarity with React.js, which was used to create this website</p>
+            <p>I am determined in my pursuit of knowledge, expanding my skillset as often as I can.  Read more about me <a href="#">here</a></p>
+
           </div>
         </header>
       </section>
