b17cbdfc6692528fa0ed66ee4faeacaf409466d4
CJ Romero <cjromero51@yahoo.com>
2018-11-13 16:26:58 -0800

    updated sidebar css

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Sidebar extends Component {
   render(){
     return (
-      <div className="sidebar">
+      <div className="nav-bar">
         <div role="button" tabIndex="1" id="hamburger" onClick={(e) => this.props.toggleOpen(e)}>
           <div className="hb-1"></div>
           <div className="hb-2"></div>
